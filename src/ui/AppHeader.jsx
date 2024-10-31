02254c7cc20a2d1d79f56310a073bcbbf00b8d5c
mainaliashish <mainaliashish@outlook.com>
2024-10-31 12:12:26 +0000

    update: - complete cart section

--- src/ui/AppHeader.jsx
+++ src/ui/AppHeader.jsx
@@ -5,7 +5,7 @@ import UserName from "../features/user/userName";
 
 const AppHeader = () => {
   return (
-    <header className="flex items-center justify-between border-b-8 border-stone-500 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
+    <header className="flex items-center justify-between bg-yellow-400 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-[5px]">
         Fast React Pizza Co.
       </Link>
